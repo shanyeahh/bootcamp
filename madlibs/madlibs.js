b47a283b4fcbd3e3425a97a8c2ad2e3b9d1d9d67
shanyeahh <shnayy@live.com>
2023-06-28 18:45:09 -0600

    what'd brandy say? almost doesn't count

--- madlibs/madlibs.js
+++ madlibs/madlibs.js
@@ -21,13 +21,18 @@ const submitMadLibs = (event) => {
 		an enormous <span class="inserted-text">${userSubmission.building}</span>. One day, a <span class="inserted-text">${userSubmission.personanimalcreature2}</span> friend came to visit, sharing
 		news about a special <span class="inserted-text">${userSubmission.magicalitem}</span>. <span class="inserted-text">${userSubmission.yourName}</span> felt very <span class="inserted-text">${userSubmission.feelingemotion}</span> to hear
 		about this and couldn't wait to see it in person, even though it was a
-		long, long journey. The friends packed up, and off they went.
-		On the way, they found a <span class="inserted-text">${userSubmission.magicalcreature1}</span> blocking the path who was crying.
+		long, long journey. The friends packed up, and off they went.</p>
+
+		<p>On the way, they found a <span class="inserted-text">${userSubmission.magicalcreature1}</span> blocking the path who was crying.
 		It was stuck in the <span class="inserted-text">${userSubmission.obstacle}</span> and couldn't get loose. So the friends
-		thought about what they could do to help. Finally, <span class="inserted-text">${userSubmission.companion}</span> had an
+		thought about what they could do to help.</p>
+
+		<p>Finally, <span class="inserted-text">${userSubmission.companion}</span> had an
 		idea! "Let's <span class="inserted-text">${userSubmission.verb}</span>!
 		After a mysterious <span class="inserted-text">${userSubmission.magicalcreature2}</span> arrived to help, it was so grateful that the magical creature
-		decided to help them, too. Soon, the friends were <span class="inserted-text">${userSubmission.verb2}</span>! They made it to
+		decided to help them, too. Soon, the friends were <span class="inserted-text">${userSubmission.verb2}</span>!</p>
+
+		<p>They made it to
 		see the <span class="inserted-text">${userSubmission.magicalitem}</span> in no time, and brought along their new magical friend!
 		When they returned home, the friends decided to <span class="inserted-text">${userSubmission.verb3}</span> in order to
 		celebrate.</p>
